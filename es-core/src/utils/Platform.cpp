57eac1664d437b7cecc39d15b0136e9a4a26486f
Teguh Sobirin <teguh@sobir.in>
2025-02-24 15:14:03 +0800

    Add SM8550 support
    Signed-off-by: Teguh Sobirin <teguh@sobir.in>

--- es-core/src/utils/Platform.cpp
+++ es-core/src/utils/Platform.cpp
@@ -427,6 +427,9 @@ namespace Utils
 					if ((Utils::String::toLower(file).find("/qcom-battery") != std::string::npos) && (batteryRootPath.empty()))
 						batteryRootPath = file;
 
+					if ((Utils::String::toLower(file).find("/qcom-battmgr-bat") != std::string::npos) && (batteryRootPath.empty()))
+						batteryRootPath = file;
+
 					if ((Utils::String::toLower(file).find("fuel") != std::string::npos) && (fuelgaugeRootPath.empty()))
 						fuelgaugeRootPath = file;
 
@@ -671,6 +674,10 @@ namespace Utils
 
 #if SM8250
 			return "sm8250";
+#endif
+
+#if SM8550
+			return "sm8550";
 #endif
 
 #endif
